4fae148c5189ae33be04af607ef016d327964ac0
Yanghao Xie <yhaoxie@gmail.com>
2019-04-27 18:31:32 +0800

    delete duplicated variable which try to fix #4

--- which-key-posframe.el
+++ which-key-posframe.el
@@ -58,11 +58,6 @@ When nil, Using current frame's font as fallback."
   :group 'which-key-posframe
   :type 'number)
 
-(defcustom which-key-posframe-height nil
-  "The height of which-key-posframe."
-  :group 'which-key-posframe
-  :type 'number)
-
 (defcustom which-key-posframe-min-width nil
   "The width of which-key-min-posframe."
   :group 'which-key-posframe
