f00f938f37eb79432bc2a6a9aa3a624832fa602a
Jonas Bernoulli <jonas@bernoul.li>
2021-05-30 21:44:07 +0200

    Do not select the which-key window and frame
    When a command is invoked while which-key is displaying the key
    bindings, then that should behave exactly the same as if which-key
    was not active.  We therefore must not change the selected window
    and frame.

--- which-key-posframe.el
+++ which-key-posframe.el
@@ -86,17 +86,19 @@ When 0, no border is showed."
 Argument ACT-POPUP-DIM includes the dimension, (height . width)
 of the buffer text to be displayed in the popup"
   (when (posframe-workable-p)
-    (posframe-show which-key--buffer
-		   :font which-key-posframe-font
-		   :position (point)
-		   :poshandler which-key-posframe-poshandler
-		   :background-color (face-attribute 'which-key-posframe :background nil t)
-		   :foreground-color (face-attribute 'which-key-posframe :foreground nil t)
-		   :height (car act-popup-dim)
-		   :width (cdr act-popup-dim)
-		   :internal-border-width which-key-posframe-border-width
-		   :internal-border-color (face-attribute 'which-key-posframe-border :background nil t)
-		   :override-parameters which-key-posframe-parameters)))
+    (save-window-excursion
+      (posframe-show
+       which-key--buffer
+       :font which-key-posframe-font
+       :position (point)
+       :poshandler which-key-posframe-poshandler
+       :background-color (face-attribute 'which-key-posframe :background nil t)
+       :foreground-color (face-attribute 'which-key-posframe :foreground nil t)
+       :height (car act-popup-dim)
+       :width (cdr act-popup-dim)
+       :internal-border-width which-key-posframe-border-width
+       :internal-border-color (face-attribute 'which-key-posframe-border :background nil t)
+       :override-parameters which-key-posframe-parameters))))
 
 (defun which-key-posframe--hide ()
   "Hide which-key buffer when posframe popup is used."
